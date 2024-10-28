9f7594e4aa807d984c069bd777dba0b95cb97ff7
KaueSilvaDev <kaue.o.silva35@gmail.com>
2024-10-28 18:29:39 -0300

    adicionando driver do mysql diretamente na classe e alterando url

--- src/main/java/school/sptech/ConexaoBD.java
+++ src/main/java/school/sptech/ConexaoBD.java
@@ -10,33 +10,29 @@ import java.sql.Statement;
 import java.sql.DriverManager;
 
 public class ConexaoBD {
-
     private final DataSource dataSource;
-
-    private String url = "jdbc:mysql://localhost:3306/";
+    private String url = "jdbc:mysql://localhost:3306/?useSSL=false&serverTimezone=UTC";
     private String username = "sptech";
     private String passwd = "123";
 
     public ConexaoBD() {
         criarBancoDeDados();
-
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl("jdbc:mysql://localhost:3306/infinity_solutions");
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/infinity_solutions?useSSL=false&serverTimezone=UTC");
         basicDataSource.setUsername(username);
         basicDataSource.setPassword(passwd);
-
         this.dataSource = basicDataSource;
     }
 
     public void criarBancoDeDados() {
-
-        try (Connection con = DriverManager.getConnection(url, username, passwd);
-             Statement statement = con.createStatement()) {
-
-            String sql = "CREATE DATABASE IF NOT EXISTS infinity_solutions;";
-            statement.executeUpdate(sql);
-
-        } catch (SQLException e) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Carregar o driver explicitamente
+            try (Connection con = DriverManager.getConnection(url, username, passwd);
+                 Statement statement = con.createStatement()) {
+                String sql = "CREATE DATABASE IF NOT EXISTS infinity_solutions;";
+                statement.executeUpdate(sql);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
